c7ee33ef0559ef7f20f0f631311959ed6f1eb76e
ofeklevii <ofeklevi@sisma.org.il>
2024-01-08 22:29:04 +0200

    exe1

--- app/src/main/java/com/example/ofekleviexe1/MainActivity.java
+++ app/src/main/java/com/example/ofekleviexe1/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.ofekleviexe1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
+    }
+
+    public void exe1(View view) {
+        btn.setText("Oh, yea, I've been clicked!");
     }
 }
